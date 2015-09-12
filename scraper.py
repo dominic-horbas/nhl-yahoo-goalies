3325e90a4cbae42a5a3d0dee424159e56a47216d
dominic-horbas <dominic.horbas@gmail.com>
2015-09-12 16:46:50 -0400

    Update pour les goalers

--- scraper.py
+++ scraper.py
@@ -10,15 +10,15 @@ def duckint(i):
         return i
 
 # Blank Python
-lookup = ['Name','GP','G','A']
-num =    [0,2,4,6]
+lookup = ['Name','GP','Wins','Shutouts', 'Avg', 'SVPct']
+num =    [0,2,8,26,18,24]
 
 lstring = ', '.join(lookup)
 
 scraperwiki.sqlite.execute('create table if not exists score (%s)'%lstring)
 
 #url='http://sports.yahoo.com/nhl/stats/byposition?pos=D'
-url='http://sports.yahoo.com/nhl/stats/byposition?pos=D&conference=NHL&year=season_2014&qualified=1'
+url='http://sports.yahoo.com/nhl/stats/byposition?pos=G&conference=NHL&year=season_2014&qualified=1'
 html=requests.get(url).content
 root=lxml.html.fromstring(html)
 
@@ -29,7 +29,6 @@ for row in rows:
     cells=[cell.text_content().strip() for cell in row.xpath('td[@class="yspscores"]')]
     for i,n in enumerate(num):
         data[lookup[i]]=duckint(cells[n])
-    data['Pts']=duckint(row.xpath('descendant-or-self::span[@class="yspscores"]')[0].text_content().strip())
     builder.append(data)
     
 scraperwiki.sqlite.save(table_name='score', data=builder, unique_keys=['Name'])
